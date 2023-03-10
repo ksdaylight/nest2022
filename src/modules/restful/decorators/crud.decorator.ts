b12e9a241003e58ba44e65784030b8b33b1f9390
ksdaylight <ksssdaylight@gmail.com>
2023-03-10 17:40:12 +0800

    to finish providers

--- src/modules/restful/decorators/crud.decorator.ts
+++ src/modules/restful/decorators/crud.decorator.ts
@@ -2,11 +2,11 @@ import { Type } from '@nestjs/common';
 
 import { BaseControllerWithTrash } from '../base';
 import { BaseController } from '../base/controller';
-import { CRUD_OPTIONS } from '../constants';
-import { CrudOptions } from '../types';
+import { CRUD_OPTIONS_REGISTER } from '../constants';
+import { CrudOptionsRegister } from '../types';
 
 export const Crud =
-    (options: CrudOptions) =>
+    (factory: CrudOptionsRegister) =>
     <T extends BaseController<any> | BaseControllerWithTrash<any>>(Target: Type<T>) => {
-        Reflect.defineMetadata(CRUD_OPTIONS, options, Target);
+        Reflect.defineMetadata(CRUD_OPTIONS_REGISTER, factory, Target);
     };
